3f2f3daaf47ac8e5ffedb43a8af209b541372027
lukeschula <schulaluke@gmail.com>
2024-11-13 22:50:04 -0600

    initialize express server

---
+++ server.js
@@ -0,0 +1,17 @@
+//Express Server with npm run start-server
+
+import express from 'express';
+import routes from './routes/index.js';
+
+// Express App Setup
+const app = express();
+const port = 5000;
+app.use(express.json())
+
+// Tie to Routes Object
+routes(app);
+
+app.listen(port);
+
+// Export
+export default app;
