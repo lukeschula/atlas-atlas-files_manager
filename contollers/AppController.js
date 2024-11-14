7e63fe5af941b7da53729c73e87fe4f0f722e62c
lukeschula <schulaluke@gmail.com>
2024-11-13 23:03:21 -0600

    Check script has created Redis and Database connectivity

---
+++ contollers/AppController.js
@@ -0,0 +1,25 @@
+// Implement status check for Redis and Database connectivity
+import redisClient from '../utils/redis.js';
+import dbClient from '../utils/db.js';
+
+class AppController {
+
+
+  // Checks if Redis/DB are active
+  static async getStatus(req, res) {
+    // Connect to both Redis and DB concurrently
+    const redisStatus = await redisClient.isAlive();
+    const dbStatus = await dbClient.isAlive();
+
+    // Return correct response based on db status
+    if (redisStatus && dbStatus) {
+      res.status(200).send({ "redis": true, "db": true });
+    } else if (!redisStatus && dbStatus) {
+      res.status(503).send('Redis Not Connected');
+    } else if (redisStatus && !dbStatus) {
+      res.status(503).send('Database Not Connected');
+    } else {
+      res.status(503).send('Unknown Database Error');
+    }
+  }
+
